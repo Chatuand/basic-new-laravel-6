a8e17c4f59109a234991ec85426b309798b68c4c
Gabriel Alvarez <gabrielalvarez_20@hotmail.com>
2022-11-10 18:53:32 -0400

    Modificacion de imagen

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,10 +5,11 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card mb-4">
+                @if($post->image)
+                <img src="{{$post->get_image}}" class="card-img-top">
+                @endif
                 <div class="card-body">
-                    @if($post->image)
-                    <img src="{{$post->get_image}}" class="card-img-top">
-                    @elseif($post->iframe)
+                    @if($post->iframe)
                     <div class="embed-responsive embed-responsive-16by9">
                         {!!$post->iframe!!}
                     </div>
